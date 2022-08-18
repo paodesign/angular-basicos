2da16f01a36ecbf0d049c2c9ac1f7fec83321c80
Paola Medina <paaaomedina@gmail.com>
2022-08-18 20:16:18 -0300

    expandiendo nuestras bases de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 import { HeroesModule } from './heroes/heroes.module';
@@ -15,7 +16,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
